f0f7e3006f1d8efd76206300a8ea277036e684ae
GIGOLO.PB <GIGOLO.PB@2d000464-569d-11de-8888-c79ed7c8b0c3>
2009-06-11 23:45:30 +0000

    atualizando

---
+++ Pybomber/src/Bomber.py
@@ -0,0 +1,21 @@
+'''
+Created on 11/06/2009
+
+@author: gigolo
+'''
+import pygame   
+
+class Bomber(pygame.sprite.Sprite):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        self.speed = 30
+        
+        
+        
