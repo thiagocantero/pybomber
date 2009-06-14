77dac25a9c87516411dfa4c492a5e24f6b614101
vitor.morato.almeida <vitor.morato.almeida@2d000464-569d-11de-8888-c79ed7c8b0c3>
2009-06-14 23:30:19 +0000

    implemantando a classe mapa

--- trunk/Pybomber/src/main.py
+++ trunk/Pybomber/src/main.py
@@ -3,8 +3,14 @@ Created on 11/06/2009
 
 @author: Vitor
 '''
-from Bomber import *
+import pygame
+from map import *
 
+screen = pygame.display.set_mode((720,624),0,32)
+m = Map()
+m.ler_arquivo()
+m.desenhar_mapa(screen)
 
-if __name__ == '__main__':
-    b = Bomber()
+while 1:
+    pygame.display.flip()
+    
